8e3244c09550b00da09a60e1b37784e60d7768b1
Rohit_ASUS <waghrohit70@gmail.com>
2022-09-11 23:41:57 +0530

    removed unwanted imports

--- src/test/java/arw.java
+++ src/test/java/arw.java
@@ -1,11 +1,17 @@
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -14,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 public class arw {
     protected static WebDriver driver;
     protected static WebDriverWait wait;
+
+    protected static Actions action;
     @BeforeMethod
     public void webSetup(){
         WebDriverManager.chromedriver().setup();
@@ -22,9 +30,11 @@ public class arw {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://seleniumbase.io/realworld");
         wait = new WebDriverWait(driver, 15);
+        action = new Actions(driver);
     }
 
     public void switchTab(int tabNumber) throws InterruptedException {
+        Thread.sleep(500);
         ArrayList<String> newTb = new ArrayList<String>(driver.getWindowHandles());
         //switch to new tab
         driver.switchTo().window(newTb.get(tabNumber));
@@ -42,11 +52,21 @@ public class arw {
         switchTab(1);
         ifMfaCodeIsValid();
         String MfaCode=GetMfaCode();
-//        System.out.println(MfaCode);
         switchTab(0);
         fillUserInfomation(MfaCode);
         logIn();
         clickOnDemoPage();
+        switchTab(2);
+        fillDemoPage();
+    }
+
+    public void getTitile() throws InterruptedException {
+        System.out.println(driver.getTitle());
+        switchTab(1);
+        driver.close();
+        switchTab(0);
+        driver.close();
+
     }
 
 
@@ -75,19 +95,91 @@ public class arw {
         driver.findElement(By.xpath("//a[@id='log-in']")).click();
     }
 
-    public void clickOnDemoPage()
-    {
+    public void clickOnDemoPage() throws InterruptedException {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Demo Page']")));
-        wait.until(ExpectedConditions.elementToBeSelected(By.xpath("//a[normalize-space()='Demo Page']")));
 
         driver.findElement(By.xpath("//a[normalize-space()='Demo Page']")).click();
+        Thread.sleep(2000);
     }
 
-    public void fillDemoPage()
+    public void checkTextFieldIsEmpty(String txt)
     {
-
+        Assert.assertTrue(txt.isEmpty());
+    }
+    public void checkTextFieldIsNotEmpty(String txt)
+    {
+        Assert.assertFalse(txt.isEmpty());
     }
 
+    public void verifyTextIsCorrect(String s1,String s2)
+    {
+        Assert.assertEquals(s1,s2);
+    }
+
+    public void fillDemoPage() throws InterruptedException {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[normalize-space()='Demo Page']")));
+        fillFirstTextInputField();
+        fillSecondTextInputField();
+        fillPrefilledTextField();
+        clickButton("Purple");
+        clickButton("Green");
+        placeHolderText();
+        clickOnSVG();
+        scrollBarFull();
+    }
+
+
+
+    public void fillFirstTextInputField(){
+        String text= driver.findElement(By.xpath("//input[@id='myTextInput']")).getText();
+        checkTextFieldIsEmpty(text);
+        driver.findElement(By.xpath("//input[@id='myTextInput']")).sendKeys("abc");
+    }
+
+    public void fillSecondTextInputField()
+    {
+        String secondText=driver.findElement(By.xpath("//textarea[@id='myTextarea']")).getText();
+        checkTextFieldIsEmpty(secondText);
+        driver.findElement(By.xpath("//textarea[@id='myTextarea']")).sendKeys("kbc");
+    }
+
+    public void fillPrefilledTextField()
+    {
+        WebElement preFilledTextFieldXpath= driver.findElement(By.xpath("//input[@id='myTextInput2']"));
+        String prefilledText=preFilledTextFieldXpath.getAttribute("value");
+        checkTextFieldIsNotEmpty(prefilledText);
+        preFilledTextFieldXpath.clear();
+        preFilledTextFieldXpath.sendKeys("Hello");
+    }
+
+    public void clickButton(String color) throws InterruptedException {
+        WebElement clickMeButton=driver.findElement(By.xpath("//button[@id='myButton']"));
+        action.moveToElement(clickMeButton).click().perform();
+        WebElement readOnlyText=driver.findElement(By.xpath("//input[@type='text' and @id='readOnlyText']"));
+        WebElement paragraphText=driver.findElement(By.xpath("//td//p[@id='pText']"));
+        verifyTextIsCorrect(clickMeButton.getText(),"Click Me ("+color+")");
+        verifyTextIsCorrect(readOnlyText.getAttribute("value"),"The Color is "+color+"");
+        verifyTextIsCorrect(paragraphText.getText(),"This Text is "+color+"");
+        Thread.sleep(1000);
+    }
+
+    public void placeHolderText(){
+        WebElement placeHolderTextXpath=driver.findElement(By.xpath("//input[@id='placeholderText']"));
+        verifyTextIsCorrect(placeHolderTextXpath.getAttribute("placeholder"),"Placeholder Text Field");
+        placeHolderTextXpath.sendKeys("sample text");
+    }
+
+    public void clickOnSVG(){
+        WebElement svgWithRect=driver.findElement(By.xpath("//*[name()='rect' and @id='svgRect']"));
+        action.moveToElement(svgWithRect).click().perform();
+    }
+
+
+    private void scrollBarFull() throws InterruptedException {
+        WebElement scrollBar=driver.findElement(By.xpath("//input[@id='mySlider']"));
+        action.moveToElement(scrollBar).clickAndHold().moveByOffset(0,150).release().perform();
+        Thread.sleep(3000);
+    }
 
     @AfterMethod
     public void closeWeb(){
